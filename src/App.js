a5f1297cff27a8107f25d7bcf307d27e8bc4bc62
fahad2021 <fahadrahman810@gmail.com>
2022-03-25 21:59:17 +0600

    create basic var

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var style={
+    color:'red',
+    backgroundColor:'green'
+  }
+  var person={
+     name:"fahad",
+     lastname:"rahman"
+  }
+  var person2={
+    name:"jonaki",
+    lastName:"rahman"
+  }
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> Hello my first react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{2+3}</h1>
+        <p  style={style}>my name is: {person.name+" "+person.lastname}</p>
+        <p style={{backgroundColor:'cyan',color:'red'}}>my crush name is {person2.name+" "+person2.lastName}</p>
       </header>
     </div>
   );
