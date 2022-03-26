10d15a38b7dfc89d40ed5a6ac15fee119043fb9c
fahad2021 <fahadrahman810@gmail.com>
2022-03-26 09:34:10 +0600

    add component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var style={
-    color:'red',
-    backgroundColor:'green'
-  }
-  var person={
-     name:"fahad",
-     lastname:"rahman"
-  }
-  var person2={
-    name:"jonaki",
-    lastName:"rahman"
-  }
+  // var style={
+  //   color:'red',
+  //   backgroundColor:'green'
+  // }
+  // var person={
+  //    name:"fahad",
+  //    lastname:"rahman"
+  // }
+  // var person2={
+  //   name:"jonaki",
+  //   lastName:"rahman"
+  // }
   return (
     <div className="App">
       
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hello my first react app
-        </p>
-        <h1>{2+3}</h1>
-        <p  style={style}>my name is: {person.name+" "+person.lastname}</p>
-        <p style={{backgroundColor:'cyan',color:'red'}}>my crush name is {person2.name+" "+person2.lastName}</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p> */}
+          {/* Edit <code>src/App.js</code> Hello my first react app */}
+        {/* </p> */}
+        {/* <h1>{2+3}</h1> */}
+        {/* <p  style={style}>my name is: {person.name+" "+person.lastname}</p> */}
+        {/* <p style={{backgroundColor:'cyan',color:'red'}}>my crush name is {person2.name+" "+person2.lastName}</p> */}
+        <Componet1 name="hi"></Componet1>
+        <Componet1></Componet1>
+        <Componet1></Componet1>
+        <Componet1></Componet1>
       </header>
     </div>
   );
+}
+//create component
+function Componet1(props){
+  var cssStyle={
+    border:'2px solid red', 
+    margin:'3px',
+    background:'cyan'
+  }
+  // return(<div style={{border:'2px solid green', margin:'3px'}}>
+  return(<div style={cssStyle}>
+    <h1>Name:Fahad</h1>
+    <p>{props.name}</p>
+    <h2>my crus is jonaki</h2>
+    <p>she is also good girl</p>
+  
+  </div>
+  )
 }
 
 export default App;
