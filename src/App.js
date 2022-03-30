380477d6cc307ea1614a7ede223b5fb915c7a0bd
fahad2021 <fahadrahman810@gmail.com>
2022-03-30 15:54:21 +0600

    file load in json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Users></Users>
         <Counter></Counter>
       </header>
     </div>
@@ -14,16 +15,31 @@ function App() {
 
 function Counter(){
   const [count,setCount]=useState(10)
-  const handleIncrease=()=>{
-    const newCount=count+1;
-    setCount(newCount)
-  };
+  const handleIncrease=()=> setCount(count+1);
+  const handleDecrease=()=> setCount(count-1);
   return <div>
     <h1>Count:{count}</h1>
-    <button onClick={handleIncrease}>Increase</button>
+    <button onMouseMove={handleIncrease}>Increase</button>
+    <button onMouseMove={handleDecrease}>Decrease</button>
   </div>
 }
 
+function Users(){
+  const [users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data));
+  },[])
+  return <div>
+    <h1>User:{users.length}</h1>
+    <ul>
+      {
+        users.map(user=><li>{user.name}</li>)
+      }
+    </ul>
+  </div>
+}
 
 
 
