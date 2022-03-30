92c6ab33ab41c2b6e91558a2da5fd91daa1f4503
fahad2021 <fahadrahman810@gmail.com>
2022-03-30 15:24:33 +0600

    create satae counter

--- src/App.js
+++ src/App.js
@@ -1,68 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products=[{name:'photoshop',price:'$99.99'},
-  {name:'illistrator',price:'$99.99'},
-  {name:'zoom',price:'$99.99'}
-
-]
-  // var style={
-  //   color:'red',
-  //   backgroundColor:'green'
-  // }
-  // var person={
-  //    name:"fahad",
-  //    lastname:"rahman"
-  // }
-  // var person2={
-  //   name:"jonaki",
-  //   lastName:"rahman"
-  // }
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p> */}
-          {/* Edit <code>src/App.js</code> Hello my first react app */}
-        {/* </p> */}
-        {/* <h1>{2+3}</h1> */}
-        {/* <p  style={style}>my name is: {person.name+" "+person.lastname}</p> */}
-        {/* <p style={{backgroundColor:'cyan',color:'red'}}>my crush name is {person2.name+" "+person2.lastName}</p> */}
-       {/* <Com name="lux"></Com>
-       <Com name="lifeboy"></Com>
-       <Com name="neem"></Com>
-       <Com name="alkatra"></Com> */}
-       <Product product={products[0]}></Product>
-       <Product product={products[1]}></Product>
-       <Product product={products[2]}></Product>
-       
-
+        <Counter></Counter>
       </header>
     </div>
   );
 }
 
-function Product(props){
-  const Productstyle={
-    border:'1px solid gray',
-    borderRadious:'5px',
-    background:'lightgray',
-    height:"200px",
-    weight:"200px",
-    float:'left'
-
-  }
-  const{name,price}= props.product
- 
-  return <div style={Productstyle}>
-    <h3>{props.product.name}</h3>
-    <h3>{props.product.price}</h3>
-    <button>Button</button>
+function Counter(){
+  const [count,setCount]=useState(10)
+  const handleIncrease=()=>{
+    const newCount=count+1;
+    setCount(newCount)
+  };
+  return <div>
+    <h1>Count:{count}</h1>
+    <button onClick={handleIncrease}>Increase</button>
   </div>
 }
-
-
 
 
 
